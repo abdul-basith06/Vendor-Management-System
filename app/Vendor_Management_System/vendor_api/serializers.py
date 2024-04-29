9fdc3cc09aeb9b7a073903d79f86b9212867f57b
abdul-basith06 <basitha85@gmail.com>
2024-04-29 10:14:53 +0530

    Serializers for all the models

---
+++ app/Vendor_Management_System/vendor_api/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Vendor, PurchaseOrder, HistoricalPerformance
+
+class VendorSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Vendor model.
+    """
+    class Meta:
+        model = Vendor
+        fields = '__all__'  
+
+class PurchaseOrderSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the PurchaseOrder model.
+    """
+    class Meta:
+        model = PurchaseOrder
+        fields = '__all__'  
+
+if HistoricalPerformance: 
+    class HistoricalPerformanceSerializer(serializers.ModelSerializer):
+        """
+        Serializer for the HistoricalPerformance model (optional).
+        """
+        class Meta:
+            model = HistoricalPerformance
+            fields = '__all__'  
