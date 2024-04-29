82616ed37b044e28808d02020271173183736013
abdul-basith06 <basitha85@gmail.com>
2024-04-29 10:34:00 +0530

    Vendor create,read,update and delete

--- app/Vendor_Management_System/vendor_api/urls.py
+++ app/Vendor_Management_System/vendor_api/urls.py
@@ -1,5 +1,8 @@
+from django.urls import path
+from .views import VendorListCreateAPIView, VendorRetrieveUpdateDestroyView
 
 
 urlpatterns = [
-    
+    path('api/vendors/', VendorListCreateAPIView.as_view(), name='vendor-create-list'),
+    path('api/vendors/<int:pk>/', VendorRetrieveUpdateDestroyView.as_view(), name='vendor-read-update-delete'),
 ]
--- app/Vendor_Management_System/vendor_api/views.py
+++ app/Vendor_Management_System/vendor_api/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Vendor
+from .serializers import VendorSerializer
+
+class VendorListCreateAPIView(ListCreateAPIView):
+    """
+    API endpoint for creating and listing vendors.
+    """
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+    permission_classes = [IsAuthenticated]
+    
+    
+
+class VendorRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    """
+    API endpoint for retrieving, updating, and deleting a specific vendor.
+    """
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        """
+        Override to filter by vendor ID from the URL segment.
+        """
+        pk = self.kwargs['vendor_id']
+        return self.get_queryset().get(pk=pk)    
