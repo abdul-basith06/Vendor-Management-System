5c11035ed5cae7bf96d4ad3df4aa4e38070161a7
abdul-basith06 <basitha85@gmail.com>
2024-04-29 11:38:22 +0530

    Added Token based urls

--- app/Vendor_Management_System/vendor_api/urls.py
+++ app/Vendor_Management_System/vendor_api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import VendorListCreateAPIView, VendorRetrieveUpdateDestroyView
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
 
 urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name="get-token"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="refresh-token"),
     path('api/vendors/', VendorListCreateAPIView.as_view(), name='vendor-create-list'),
     path('api/vendors/<int:pk>/', VendorRetrieveUpdateDestroyView.as_view(), name='vendor-read-update-delete'),
 ]
