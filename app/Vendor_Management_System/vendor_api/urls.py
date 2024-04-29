1155933b90143cecda26dc3145f6f3a9bbaa94c2
abdul-basith06 <basitha85@gmail.com>
2024-04-29 12:32:44 +0530

    PurchaseOrder model create,read,update,delete

--- app/Vendor_Management_System/vendor_api/urls.py
+++ app/Vendor_Management_System/vendor_api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import VendorListCreateAPIView, VendorRetrieveUpdateDestroyView
+from .views import VendorListCreateAPIView, VendorRetrieveUpdateDestroyView, PurchaseOrderListCreateAPIView, PurchaseOrderRetrieveUpdateDestroyView
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -8,6 +8,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="get-token"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh-token"),
-    path('api/vendors/', VendorListCreateAPIView.as_view(), name='vendor-create-list'),
-    path('api/vendors/<int:pk>/', VendorRetrieveUpdateDestroyView.as_view(), name='vendor-read-update-delete'),
+    path('vendors/', VendorListCreateAPIView.as_view(), name='vendor-create-list'),
+    path('vendors/<int:pk>/', VendorRetrieveUpdateDestroyView.as_view(), name='vendor-read-update-delete'),
+    path('purchase_orders/', PurchaseOrderListCreateAPIView.as_view(), name='purchase-order-create-list'),
+    path('purchase_orders/<int:po_id>/', PurchaseOrderRetrieveUpdateDestroyView.as_view(), name='purchase-order-read-update-delete'),
+
 ]
--- app/Vendor_Management_System/vendor_api/views.py
+++ app/Vendor_Management_System/vendor_api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import OrderingFilter
+from rest_framework.not_found import NotFound # type: ignore
+from django.db.models import Q
 
-from .models import Vendor
-from .serializers import VendorSerializer
+from .models import Vendor, PurchaseOrder
+from .serializers import VendorSerializer, PurchaseOrderSerializer
 
 class VendorListCreateAPIView(ListCreateAPIView):
     """
@@ -11,7 +16,6 @@ class VendorListCreateAPIView(ListCreateAPIView):
     queryset = Vendor.objects.all()
     serializer_class = VendorSerializer
     permission_classes = [IsAuthenticated]
-    
     
 
 class VendorRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
@@ -28,3 +32,61 @@ class VendorRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
         """
         pk = self.kwargs['vendor_id']
         return self.get_queryset().get(pk=pk)    
+    
+    
+class PurchaseOrderListCreateAPIView(ListCreateAPIView):
+    """
+    API endpoint to list and create purchase orders with vendor filtering.
+    """
+    queryset = PurchaseOrder.objects.all()
+    serializer_class = PurchaseOrderSerializer
+    filter_backends = [OrderingFilter]  
+
+    def get(self, request):
+        """
+        Retrieve a list of purchase orders.
+        """
+        queryset = self.get_queryset()
+        vendor_id = request.query_params.get('vendor')
+        if vendor_id:
+            queryset = queryset.filter(Q(vendor__id=vendor_id))
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def get_queryset(self):
+        """
+        Override to filter by vendor (optional parameter in query string).
+        """
+        queryset = self.queryset
+        vendor_id = self.request.query_params.get('vendor')
+        if vendor_id:
+            queryset = queryset.filter(Q(vendor__id=vendor_id))
+        return queryset
+
+    def post(self, request):
+        """
+        Create a purchase order.
+        """
+        serializer = PurchaseOrderSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    
+class PurchaseOrderRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    """
+    API endpoint to retrieve, update, and delete a specific purchase order.
+    """
+    queryset = PurchaseOrder.objects.all()
+    serializer_class = PurchaseOrderSerializer
+
+    def get_object(self):
+        """
+        Override to handle 404 (Not Found) for missing purchase orders.
+        """
+        pk = self.kwargs.get('po_id')
+        try:
+            return self.queryset.get(pk=pk)
+        except PurchaseOrder.DoesNotExist:
+            raise NotFound('Purchase order with ID ' + str(pk) + ' not found.')    
+    
