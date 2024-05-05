4524df17fb8c8933f2eef25d7bd0a31a718bb4c1
abdul-basith06 <basitha85@gmail.com>
2024-05-05 11:38:50 +0530

    Changes access token expiry

--- app/Vendor_Management_System/Vendor_Management_System/settings.py
+++ app/Vendor_Management_System/Vendor_Management_System/settings.py
@@ -40,7 +40,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=24),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
 }
 
